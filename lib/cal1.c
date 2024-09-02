de32199a2c7280c5163bbb12d7361892b7a6552b
keshav <keshav1787.be22@chitkara.edu.in>
2024-09-02 15:38:23 +0530

    library implementation

---
+++ lib/cal1.c
@@ -0,0 +1,28 @@
+int min(int a,int b){
+	if( a<b){
+		retrun a;
+		}
+	return b;
+}
+int max(int a,int b){
+	if(b>a){
+		return b;
+	}
+	return a;
+}
+int abs(int a){
+	if( a<0){
+		return (0-a);
+	}
+	return a;
+}
+
+
+int mod(int a,int b){
+	return a%b;
+}
+
+
+
+
+
---
+++ lib/cal1.h
@@ -0,0 +1,5 @@
+int mod(int a,int b);
+int min(int a,int b);
+int max(int a,int b);
+int abs(int a);
+
